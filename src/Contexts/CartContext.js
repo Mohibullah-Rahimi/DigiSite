2dda6a8fc02009ee2fde68fc1981306fb692d0f4
Mohibullah Rahimi <mohibullah786.ghor@gmail.com>
2024-05-28 16:59:46 -0500

    adding card context.js

--- src/Contexts/CartContext.js
+++ src/Contexts/CartContext.js
@@ -15,47 +15,57 @@ const initialState = {
 };
 
 const CartContextProvider = ({ children }) => {
-    const { products} = useContext(ProductsContext);
+    const { products } = useContext(ProductsContext);
     const [state, dispatch] = useReducer(cart_reducer, initialState);
     
+    // Effect to load products into the cart
     useEffect(() => {
         dispatch({ type: "LOAD_PRODUCTS", payload: products });
     }, [products]);
 
+    // Effect to update cart count, subtotal, and total when the cart changes
     useEffect(() => {
         dispatch({ type: "UPDATE_CART_COUNT" });
         dispatch({ type: "CART_SUBTOTAL" });
-         dispatch({ type: "CART_TOTAL" });
+        dispatch({ type: "CART_TOTAL" });
     }, [state.cart]);
 
+    // Function to handle adding an item to the cart
     const handleAddToCart = (id) => {
         dispatch({ type: "ADD_TO_CART", payload: id });
     }
 
+    // Function to decrease the count of an item in the cart
     const decreaseCartCount = (id) => {
         dispatch({ type: "DECREASE_COUNT", payload: id });
     }
 
+    // Function to remove an item from the cart
     const removeItemFromCart = (id) => {
-        dispatch({type: "REMOVE_ITEM", payload:id})
+        dispatch({ type: "REMOVE_ITEM", payload: id });
     }
 
+    // Function to clear the entire cart
     const clearCart = () => {
         dispatch({ type: "CLEAR_CART" });
     }
 
+    // Function to recalculate the subtotal
     const getSubtotal = () => {
         dispatch({ type: "CART_SUBTOTAL" });
     }
 
+    // Function to recalculate the total
     const getTotal = () => {
         dispatch({ type: "CART_TOTAL" });
     }
 
+    // Function to save the user's email
     const saveEmail = (email) => {
         dispatch({ type: "SAVE_EMAIL", payload: email });
     }
 
+    // Providing the context value with state and functions for manipulating the cart
     return (
       <CartContext.Provider
         value={{ ...state, handleAddToCart, decreaseCartCount, clearCart, removeItemFromCart, getSubtotal, getTotal, saveEmail }}>
